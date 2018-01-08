dede9e333f74575538e3e958398bb0c09460a7fa
liansenyao <18668196356@163.com>
2018-01-08 11:54:13 +0800

    噶电话

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by jiechu on 2018/1/8.
+//  Copyright © 2018年 J.C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by jiechu on 2018/1/8.
+//  Copyright © 2018年 J.C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
